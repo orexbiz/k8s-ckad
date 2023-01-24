f220bc91d9c62f7dd2219c59cf2859a58a8522a9
orex <ismartr@yahoo.fr>
2023-01-24 18:29:03 +0100

    cleanup script

--- iac/ansible/play.sh
+++ iac/ansible/play.sh
@@ -4,12 +4,12 @@ set -e
 
 INVENTORY_FILE="inventory.ansible.yaml"
 PLAYBOOK_FILE="playbook.ansible.yaml"
-IAC_DIR="../iac-for-labs"
+
+cd "$( dirname ${BASH_SOURCE[0]})"
 
 if [ ! -f "${INVENTORY_FILE}" -o ! -f "${PLAYBOOK_FILE}" ]; then
-    echo "[ERROR]: missing inventory file"
+    echo "[ERROR]: missing usefull files (${INVENTORY_FILE},${PLAYBOOK_FILE})"
     exit 1
 fi
 
-cd "$( dirname ${BASH_SOURCE[0]})"
-ansible-playbook -vv -i ${INVENTORY_FILE} ${PLAYBOOK_FILE} --extra-vars "NAME=$1" --extra-vars "PASSWD=$2"
+ansible-playbook -vvvv -i ${INVENTORY_FILE} ${PLAYBOOK_FILE}  #--extra-vars "NAME=$1" --extra-vars "PASSWD=$2"
